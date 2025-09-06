fb2569c3ed0a65f5a1a24cb90d6db2bc5336b6bb
nurhatmurathan <nurhatmurathan001@gmail.com>
2025-09-06 16:34:39 +0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,7 @@ export default defineConfig({
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
+    preview: {
+        allowedHosts: ["optims-rating.vercel.app"],
+    },
 });
